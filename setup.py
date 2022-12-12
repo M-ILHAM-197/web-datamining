6ac4fc956994edee1d64ea91bf4beebf3a8ba7e9
M. Ilham Anggis Bangkit Pamungkas <83936039+M-ILHAM-197@users.noreply.github.com>
2022-12-12 11:32:49 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,6 +11,8 @@ from sklearn.impute import SimpleImputer
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.model_selection import train_test_split
 
 st.set_page_config(page_title="Aplikasi Data Mining", page_icon=":computer:", layout="wide")
 
@@ -83,8 +85,95 @@ with tab2:
 
     st.write("Setelah dinormalisasi")
     st.write(dataset.head(10))
+    
+    penguin_raw.loc[(penguin_raw['species'] == "Adelie"), "species"] = 0
+    penguin_raw.loc[(penguin_raw['species'] == "Gentoo"), "species"] = 1
+    penguin_raw.loc[(penguin_raw['species'] == "Chinstrap"), "species"] = 2
+
+    penguin_raw.loc[(penguin_raw['island'] == "Torgersen"), "island"] = 0
+    penguin_raw.loc[(penguin_raw['island'] == "Biscoe"), "island"] = 1
+    penguin_raw.loc[(penguin_raw['island'] == "Dream"), "island"] = 2
+
+    penguin_raw['sex'] = penguin_raw['sex'].replace({'male':1, 'female':0})
+
+    data = penguin_raw.drop(columns=["bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g"])
+
+    st.dataframe(data.head(10))
+
+    st.subheader("Preprocessing Data Berhasil")
+
+    dataset1 = data
+    dataset1 = dataset1.join(dataset)
+
+    st.dataframe(dataset)
 with tab3:
     st.header("Modelling")
+    
+    randomforest_cekbox = st.checkbox("Random Forest")
+    knn_cekbox = st.checkbox("KNN")
+    bayes_gaussian_cekbox = st.checkbox("Naive-Bayes Gaussian")
+    decission3_cekbox = st.checkbox("Decission Tree")
+
+    #=========================== Spliting data ======================================
+    X = dataset.iloc[:,2:-1]
+    Y = penguin_raw.iloc[:,-1]
+
+    X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size=0.25, random_state=0)    
+
+    #============================ Model =================================
+
+    randomforest = RandomForestClassifier()
+    randomforest.fit(X_train, Y_train)
+    y_pred_randomforest = randomforest.predict(X_test)
+    randomforest_accuracy = round(100 * accuracy_score(Y_test, y_pred_randomforest), 2)
+    #===================== KNN =======================
+
+    knn = KNeighborsClassifier()
+    knn.fit(X_train, Y_train)
+    y_predknn = knn.predict(X_test)
+    knn_accuracy = round(100 * accuracy_score(Y_test, y_predknn), 2)
+
+    #===================== Bayes Gaussian =============
+    gaussian = GaussianNB()
+    gaussian.fit(X_train,Y_train)
+    y_pred_gaussian   =  gaussian.predict(X_test)
+    gauss_accuracy  = round(100*accuracy_score(Y_test, y_pred_gaussian),2)
+    gaussian_eval = classification_report(Y_test, y_pred_gaussian,output_dict = True)
+    gaussian_eval_df = pd.DataFrame(gaussian_eval).transpose()
+
+    #===================== Decission tree =============
+    decission3  = DecisionTreeClassifier(criterion="gini")
+    decission3.fit(X_train,Y_train)
+    y_pred_decission3 = decission3.predict(X_test)
+    decission3_accuracy = round(100*accuracy_score(Y_test, y_pred_decission3),2)
+    decission3_eval = classification_report(Y_test, y_pred_decission3,output_dict = True)
+    decission3_eval_df = pd.DataFrame(decission3_eval).transpose()
+
+    st.markdown("---")
+
+    #===================== Cek Box ====================
+    if randomforest_cekbox:
+        st.write("##### Random Forest Classifier")
+        st.error("Dengan menggunakan metode Random Forest didapatkan akurasi sebesar:")
+        st.error(f"Akurasi = {randomforest_accuracy}%")
+
+    if knn_cekbox:
+        st.write("##### KNN")
+        st.warning("Dengan menggunakan metode KNN didapatkan akurasi sebesar:")
+        # st.warning(knn_accuracy)
+        st.warning(f"Akurasi  =  {knn_accuracy}%")
+        st.markdown("---")
+
+    if bayes_gaussian_cekbox:
+        st.write("##### Naive Bayes Gausssian")
+        st.info("Dengan menggunakan metode Bayes Gaussian didapatkan hasil akurasi sebesar:")
+        st.info(f"Akurasi = {gauss_accuracy}%")
+        st.markdown("---")
+
+    if decission3_cekbox:
+        st.write("##### Decission Tree")
+        st.success("Dengan menggunakan metode Decission tree didapatkan hasil akurasi sebesar:")
+        st.success(f"Akurasi = {decission3_accuracy}%")
 
 with tab4:
     st.header("Implementasi")
